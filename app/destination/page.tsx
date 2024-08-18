5f208e82af6540a8d29dde4f9a96894dcf84c65c
HireMeFullTime <2017pracownik@gmail.com>
2024-08-18 02:41:41 +0200

    Removed helper outlines, improved styling (destination page)

--- app/destination/page.tsx
+++ app/destination/page.tsx
@@ -32,7 +32,7 @@ const Destination = () => {
         <span className="pr-2 font-bold opacity-25 xl:pr-4">01</span> PICK YOUR
         DESTINATION
       </h1>
-      <div className="outline outline-1 outline-green-300 xl:-ml-[90px] xl:flex xl:gap-40 xl:pt-16">
+      <div className="xl:-ml-[90px] xl:flex xl:gap-40 xl:pt-16">
         <div className="mb-[26px] mt-8 flex justify-center md:mb-[50px] md:mt-[60px] ">
           <Image
             src={imagesMap[activeTab]}
@@ -42,10 +42,10 @@ const Destination = () => {
             className="md:h-[300px] md:w-[300px] xl:h-[445px] xl:w-[445px]"
           />
         </div>
-        <div className="flex flex-col items-center outline outline-1 outline-yellow-100 xl:max-w-[445px] xl:items-start">
+        <div className="flex flex-col items-center xl:max-w-[445px] xl:items-start">
           {/* tabs titles*/}
 
-          <div className="flex gap-7 text-sm xl:gap-9 xl:text-base ">
+          <div className="flex gap-7 text-sm xl:gap-9 xl:text-base">
             {destinations.map((destination) => (
               <button
                 key={destination.name}
@@ -68,25 +68,25 @@ const Destination = () => {
               activeTab === destination.name && (
                 <div
                   key={destination.name}
-                  className="flex flex-col items-center xl:items-start"
+                  className="flex flex-col items-center md:max-w-[573px] xl:items-start"
                 >
-                  <h2 className="pt-5 xl:pt-10 font-bellefair text-[56px] uppercase leading-[64px] text-white  md:text-[80px] md:leading-[92px] xl:text-[100px] xl:leading-[115px]">
+                  <h2 className="pt-5 font-bellefair text-[56px] uppercase leading-[64px] text-white md:text-[80px]  md:leading-[92px] xl:pt-10 xl:text-[100px] xl:leading-[115px]">
                     {destination.name}
                   </h2>
-                  <p className="text-center font-barlow text-[15px] leading-[25px] text-[#D0D6F9] md:px-[78px] md:pt-[10px] md:text-base md:leading-7 xl:px-0 xl:text-left xl:text-lg xl:leading-8">
+                  <p className="text-center font-barlow text-[15px] leading-[25px] text-[#D0D6F9] md:px-[63px] md:pt-[10px] md:text-base md:leading-7 xl:px-0 xl:text-left xl:text-lg xl:leading-8">
                     {destination.description}
                   </p>
                   <hr className="my-8 w-full border-[#383B4B] md:mt-12" />
                   <div className="md:flex">
-                    <p className="flex max-w-[216px] flex-wrap justify-center text-center font-barlow_condensed text-sm uppercase leading-[17px] tracking-[2.36px] text-[#D0D6F9] outline outline-1 outline-red-200">
+                    <p className="flex max-w-[216px] flex-wrap justify-center text-center font-barlow_condensed text-sm uppercase leading-[17px] tracking-[2.36px] text-[#D0D6F9]">
                       AVG. Distance
                       <span className="mb-8 mt-3 font-bellefair text-[28px] leading-8 text-white md:mb-0">
                         {destination.distance}
                       </span>
                     </p>
-                    <p className="flex max-w-[216px] flex-wrap justify-center text-center font-barlow_condensed text-sm uppercase leading-[17px] tracking-[2.36px] text-[#D0D6F9]  outline outline-1 outline-red-200">
+                    <p className="flex max-w-[216px] flex-wrap justify-center text-center font-barlow_condensed text-sm uppercase leading-[17px] tracking-[2.36px] text-[#D0D6F9]">
                       EST. Travel Time
-                      <span className="mb-8 mt-3 font-bellefair text-[28px] leading-8 text-white md:mb-0">
+                      <span className="mt-3 font-bellefair text-[28px] leading-8 text-white md:mb-0">
                         {destination.travel}
                       </span>
                     </p>
