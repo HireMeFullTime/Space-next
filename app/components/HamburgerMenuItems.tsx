22647548150fa676834582dc4904f67681275e1d
HireMeFullTime <2017pracownik@gmail.com>
2024-10-08 17:52:50 +0200

    Css improvements (technology dots)

--- app/components/HamburgerMenuItems.tsx
+++ app/components/HamburgerMenuItems.tsx
@@ -11,13 +11,12 @@ interface MenuItemsProps {
 
 const HamburgerMenuItems: React.FC<MenuItemsProps> = ({ closeMenu }) => {
   const pathname = usePathname();
-  // overflow-y-auto
   return (
     <nav className="animate-slide-in fixed right-0 top-0 z-[100] flex h-screen w-64 max-w-64 flex-col gap-16  bg-white/5 pl-8 pr-0 pt-8 text-white  backdrop-blur-xl supports-[not(backdrop-filter:blur(1px))]:bg-[#1c1a26]">
       <button onClick={closeMenu} className="self-end pr-7">
         <Image src={close_icon} alt="Close icon" width={20} height={20} />
       </button>
-      <ul className='overflow-y-auto'>
+      <ul className="overflow-y-auto">
         {navLinks.map((item) => (
           <li key={item.id} className="relative pb-8 tracking-[2.7px]">
             <Link
